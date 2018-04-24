32d8ff93aa834dadc7cc7a395e90a30b8eccfa4f
Patrick Schuelke <p-schuelke@gmx.de>
2018-04-24 13:42:57 +0200

    add boilerplate PostList component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import PostList from "./PostList";
 
 class App extends Component {
   render() {
-    return <div>App is working</div>;
+    return (
+      <div className="container">
+        <h1> TODO - List</h1>
+        <PostList />
+      </div>
+    );
   }
 }
 
---
+++ src/PostList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PostList = props => {
+  return (
+    <div>
+      <ul className="card">
+        <li>This is a post</li>
+      </ul>
+    </div>
+  );
+};
+
+export default PostList;
