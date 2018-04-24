0985107bd4f69623a070d66fb0879cf91e3e9a63
Unknown <ceger1985@gmail.com>
2018-04-24 14:34:29 +0200

    Auto stash before merge of "master" and "origin/master"
    move-creatpost

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import PostList from "./PostList";
-
+import CreatePost from "./CreatePost";
 class App extends Component {
   render() {
     return (
       <div className="container">
         <h1> TODO - List</h1>
+        <CreatePost />
         <PostList />
+        
       </div>
     );
   }
