4263765c240b94994980a5e972b7e34524f56847
Patrick Schuelke <p-schuelke@gmx.de>
2018-04-25 09:29:26 +0200

    refactored to functional component

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,20 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import "./ToDoItem.css";
 
-class ToDoItem extends Component {
-  render() {
-    return (
-      <li className="collection-item todo-item">
-        <span>{this.props.todoText}</span>
-        <button
-          className="waves-effect waves-light btn btn-small todo-delete"
-          onClick={() => this.props.deleteToDo(this.props.created)}
-        >
-          Delete
-        </button>
-      </li>
-    );
-  }
-}
+const ToDoItem = props => (
+  <li className="collection-item todo-item">
+    <span>{props.todoText}</span>
+    <button
+      className="waves-effect waves-light btn btn-small todo-delete"
+      onClick={() => props.deleteToDo(props.created)}
+    >
+      Delete
+    </button>
+  </li>
+);
 
 export default ToDoItem;
