eb3525e878acbceddb580bbe66896c6fdc8c857e
Patrick Schuelke <p-schuelke@gmx.de>
2018-04-25 09:38:36 +0200

    todo items can now be marked as done

--- src/PostList.js
+++ src/PostList.js
@@ -1,21 +1,21 @@
 import React from "react";
 import ToDoItem from "./ToDoItem";
 
-const PostList = props => {
-  return (
-    <div className="card">
-      <ul className="collection">
-        {props.todoList.map(todo => (
-          <ToDoItem
-            key={todo.created}
-            todoText={todo.text}
-            deleteToDo={props.deleteToDo}
-            created={todo.created}
-          />
-        ))}
-      </ul>
-    </div>
-  );
-};
+const PostList = props => (
+  <div className="card">
+    <ul className="collection">
+      {props.todoList.map(todo => (
+        <ToDoItem
+          key={todo.created}
+          todoText={todo.text}
+          deleteToDo={props.deleteToDo}
+          markAsDone={props.markAsDone}
+          created={todo.created}
+          done={todo.done}
+        />
+      ))}
+    </ul>
+  </div>
+);
 
 export default PostList;
--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -2,8 +2,14 @@ import React from "react";
 import "./ToDoItem.css";
 
 const ToDoItem = props => (
-  <li className="collection-item todo-item">
-    <span>{props.todoText}</span>
+  <li className={`collection-item todo-item ${props.done && "teal lighten-4"}`}>
+    <button
+      className="waves-effect waves-light btn btn-small todo-delete"
+      onClick={() => props.markAsDone(props.created)}
+    >
+      Done
+    </button>
+    <span> {props.todoText}</span>
     <button
       className="waves-effect waves-light btn btn-small todo-delete"
       onClick={() => props.deleteToDo(props.created)}
