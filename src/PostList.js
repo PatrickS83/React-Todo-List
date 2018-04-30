d199dbeb81101ca36f936dba6bca5cc1bb975023
Patrick Schuelke <p-schuelke@gmx.de>
2018-04-30 12:11:17 +0200

    refactored map function for react transition group

--- src/PostList.js
+++ src/PostList.js
@@ -1,26 +1,28 @@
 import React from "react";
+import { CSSTransitionGroup } from "react-transition-group";
 import ToDoItem from "./ToDoItem";
 
-const PostList = props => (
-  <div className="card">
-    <ul className="collection">
-      {props.todoList.length
-        ? props.todoList.map((todo, i) => (
-            <ToDoItem
-              key={todo.created}
-              todoList={props.todoList}
-              todoText={todo.text}
-              deleteToDo={props.deleteToDo}
-              created={todo.created}
-              markAsDone={props.markAsDone}
-              done={todo.done}
-              index={i}
-              moveItem={props.moveItem}
-            />
-          ))
-        : null}
-    </ul>
-  </div>
-);
+const PostList = props => {
+  const todoItems = props.todoList.length
+    ? props.todoList.map((todo, i) => (
+        <ToDoItem
+          key={todo.created}
+          todoList={props.todoList}
+          todoText={todo.text}
+          deleteToDo={props.deleteToDo}
+          created={todo.created}
+          markAsDone={props.markAsDone}
+          done={todo.done}
+          index={i}
+          moveItem={props.moveItem}
+        />
+      ))
+    : null;
+  return (
+    <div className="card">
+      <ul className="collection">{todoItems}</ul>
+    </div>
+  );
+};
 
 export default PostList;
